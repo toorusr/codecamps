def35081d194c49dac828a7ddc5862f4c0e799ac
Fronbasal <mail@fronbasal.de>
2017-06-22 19:57:01 +0200

    Implemented workshops and refactored code a bit

--- server.go
+++ server.go
@@ -5,7 +5,7 @@ import "github.com/PuerkitoBio/goquery"
 import "strings"
 import "strconv"
 
-type Camp struct {
+type Event struct {
 	Ort   string `json:"ort"`
 	Datum string `json:"datum"`
 }
@@ -31,8 +31,8 @@ func main() {
 		c.JSON(200, camps)
 	})
 
-	m.GET("/camp/:camp", func(c *gin.Context) {
-		index, err1 := strconv.Atoi(c.Param("camp"))
+	m.GET("/camp/:index", func(c *gin.Context) {
+		index, err1 := strconv.Atoi(c.Param("index"))
 		camp, err2 := fetchCamps()
 		if err1 != nil || err2 != nil {
 			c.JSON(500, gin.H{
@@ -51,17 +51,61 @@ func main() {
 		}
 	})
 
+	m.GET("/workshops", func(c *gin.Context) {
+		workshops, err := fetchWorkshops()
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error":   true,
+				"message": "Could not fetch data",
+			})
+			return
+		}
+		c.JSON(200, workshops)
+	})
+
+	m.GET("/workshop/:index", func(c *gin.Context) {
+		index, err1 := strconv.Atoi(c.Param("index"))
+		workshop, err2 := fetchCamps()
+		if err1 != nil || err2 != nil {
+			c.JSON(500, gin.H{
+				"error":   true,
+				"message": "Could not fetch data",
+			})
+			return
+		}
+		if index <= len(workshop) {
+			c.JSON(200, workshop[index])
+		} else {
+			c.JSON(400, gin.H{
+				"error":   true,
+				"message": "Invalid index!",
+			})
+		}
+	})
+
 	m.Run(":5000")
 }
 
-func fetchCamps() ([]Camp, error) {
+func fetchCamps() ([]Event, error) {
 	doc, err := goquery.NewDocument("https://code.design/camps/")
 	if err != nil {
 		return nil, err
 	}
-	camps := []Camp{}
+	camps := []Event{}
 	doc.Find("section.box ul li").Not(".pdf").Each(func(i int, s *goquery.Selection) {
-		camps = append(camps, Camp{Datum: strings.SplitAfter(s.Text(), ",")[0], Ort: strings.SplitAfter(s.Text(), ",")[1]})
+		camps = append(camps, Event{Datum: strings.Replace(strings.SplitAfter(s.Text(), ",")[0], ",", "", -1), Ort: strings.Replace(strings.SplitAfter(s.Text(), ",")[1], ",", "", -1)})
 	})
 	return camps, nil
+}
+
+func fetchWorkshops() ([]Event, error) {
+	doc, err := goquery.NewDocument("https://code.design/workshops/")
+	if err != nil {
+		return nil, err
+	}
+	workshops := []Event{}
+	doc.Find("section.box ul li").Each(func(i int, s *goquery.Selection) {
+		workshops = append(workshops, Event{Datum: strings.Replace(strings.SplitAfter(s.Text(), ",")[0], ",", "", -1), Ort: strings.Replace(strings.SplitAfter(s.Text(), ",")[1], ",", "", -1)})
+	})
+	return workshops, nil
 }
